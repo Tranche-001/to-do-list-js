59861953249a640c315f9f20bf5eb0dd8f36a2e4
Tranche-001 <tranche100@gmail.com>
2025-07-14 13:37:50 -0300

    add TodoItem class

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-class Todo {
-  constructor(title){
-
+class TodoItem {
+  constructor(title, description, dueDate, priority, completed){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = completed;
   }
+ 
 }
